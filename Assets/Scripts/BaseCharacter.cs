823f9fc6c66161184f1acb36fedd35efc0fc905d
Mrdosim <127918879+Mrdosim@users.noreply.github.com>
2024-09-02 16:50:22 +0900

    feat/add_assets_6

---
+++ Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacter : MonoBehaviour
+{
+    protected Transform characterPos;
+    // µµÂøÁö´Â + Vector.right * 3
+
+    void Move()
+    {
+
+    }
+}
