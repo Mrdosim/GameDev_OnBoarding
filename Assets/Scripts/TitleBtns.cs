bd0b00010cc1279d954bade8fa1b0e42b34b2b80
Mrdosim <127918879+Mrdosim@users.noreply.github.com>
2024-09-05 18:12:37 +0900

    feat / Title

---
+++ Assets/Scripts/TitleBtns.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleBtns:MonoBehaviour
+{
+    public Button startBtn;
+    public Button exitBtn;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
