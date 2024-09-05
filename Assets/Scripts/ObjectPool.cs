22188982d92af9cf8431e22db15e7f755b51a8da
Mrdosim <127918879+Mrdosim@users.noreply.github.com>
2024-09-05 13:57:48 +0900

    fix / objectpool

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,42 +1,98 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class PoolItem
+{
+    public string prefabName; // 프리팹의 이름을 식별하기 위한 필드
+    public GameObject prefab; // 프리팹 자체
+    public int initialSize = 10; // 초기 풀 크기
+}
+
 public class ObjectPool : MonoBehaviour
 {
-    public GameObject prefab; // 생성할 프리팹
-    public int initialSize = 10; // 초기 풀 크기
+    public static ObjectPool Instance { get; private set; }
 
-    private Queue<GameObject> pool = new Queue<GameObject>();
+    public List<PoolItem> poolItems; // 여러 프리팹을 관리할 리스트
 
-    void Start()
+    private Dictionary<string, Queue<GameObject>> poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+    private void Awake()
     {
-        for (int i = 0; i < initialSize; i++)
+        if (Instance == null)
         {
-            GameObject obj = Instantiate(prefab);
-            obj.SetActive(false);
-            pool.Enqueue(obj);
-        }
-    }
-
-    public GameObject GetObject()
-    {
-        if (pool.Count > 0)
-        {
-            GameObject obj = pool.Dequeue();
-            obj.SetActive(true);
-            return obj;
+            Instance = this;
         }
         else
         {
-            GameObject obj = Instantiate(prefab);
-            return obj;
+            Destroy(gameObject);
         }
     }
 
-    public void ReturnObject(GameObject obj)
+    void Start()
+    {
+        InitializePools();
+    }
+
+    void InitializePools()
+    {
+        foreach (var item in poolItems)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < item.initialSize; i++)
+            {
+                GameObject obj = Instantiate(item.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(item.prefabName, objectPool); // 프리팹 이름으로 큐를 구분
+        }
+    }
+
+    // 특정 프리팹 이름에 맞는 오브젝트 가져오기
+    public GameObject GetObject(string prefabName)
+    {
+        if (poolDictionary.ContainsKey(prefabName))
+        {
+            if (poolDictionary[prefabName].Count > 0)
+            {
+                GameObject obj = poolDictionary[prefabName].Dequeue();
+                obj.SetActive(true);
+                return obj;
+            }
+            else
+            {
+                // 풀이 비어있으면 새로운 오브젝트 생성
+                PoolItem poolItem = poolItems.Find(item => item.prefabName == prefabName);
+                if (poolItem != null)
+                {
+                    GameObject newObj = Instantiate(poolItem.prefab);
+                    return newObj;
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError($"Prefab with name {prefabName} not found in pool.");
+        }
+
+        return null;
+    }
+
+    // 오브젝트를 풀에 반환
+    public void ReturnObject(GameObject obj, string prefabName)
     {
         obj.SetActive(false);
-        pool.Enqueue(obj);
+
+        if (poolDictionary.ContainsKey(prefabName))
+        {
+            poolDictionary[prefabName].Enqueue(obj);
+        }
+        else
+        {
+            Debug.LogError($"Prefab with name {prefabName} not found in pool.");
+        }
     }
 }
-
