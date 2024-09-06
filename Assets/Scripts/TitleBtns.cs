f806db82f0a87c726066300a54235edbf451651a
Mrdosim <127918879+Mrdosim@users.noreply.github.com>
2024-09-06 12:09:00 +0900

    Update TitleBtns.cs

--- Assets/Scripts/TitleBtns.cs
+++ Assets/Scripts/TitleBtns.cs
@@ -7,11 +7,6 @@ public class TitleBtns:MonoBehaviour
     public Button startBtn;
     public Button exitBtn;
 
-    public void StartGame()
-    {
-        SceneManager.LoadScene("MainScene");
-    }
-
     public void ExitGame()
     {
         Application.Quit();
